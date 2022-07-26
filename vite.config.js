fe400e6af335491487011dba8627903f225c3bbd
Kamerr Ezz <kamerr_oficial@hotmail.com>
2022-07-26 16:18:43 -0500

    change name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo-trello-vue',
+  base: '/todo-list-vue',
   plugins: [vue()],
   resolve: {
     alias: {
